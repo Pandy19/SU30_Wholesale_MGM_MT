861aba8290cfb6cca682190b4fac0e0cc7da1bd1
Pandy19 <Hoeng.sophanafc@gmail.com>
2025-12-28 10:58:06 +0700

    Update header.blade.php

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -32,7 +32,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{asset('assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{asset('assets/dist/img/MMOLOGO2.png')}}" alt="AdminLTELogo" height="100" width="100">
   </div>
 
   <!-- Navbar -->
